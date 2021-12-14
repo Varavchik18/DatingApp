0665588259da91b0c19b373ad7c9bcceafecfa3f
Varavchik18 <48031083+Varavchik18@users.noreply.github.com>
2021-12-15 01:31:13 +0200

    Added EF core, async methods, api controller ("UserController")

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+        
+    }
+}
