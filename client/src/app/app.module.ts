3fe22fc722d5b5347314a456c914c88adfaa6e31
Varavchik18 <48031083+Varavchik18@users.noreply.github.com>
2022-01-05 15:10:50 +0200

    End of section 6 Client side. Added theme, setted guards, and routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -12,6 +12,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
 
 
 @NgModule({
@@ -19,7 +25,11 @@ import { RegisterComponent } from './register/register.component';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +37,7 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,7 +14,8 @@ export class NavComponent implements OnInit {
   model: any= {  };
   loggedIn: boolean; 
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router:Router, 
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -20,13 +23,15 @@ export class NavComponent implements OnInit {
     Login(){
         this.accountService.Login(this.model).subscribe(response=>
           {
-            console.log(response);
+            this.router.navigateByUrl('/members')
           }, error =>{
             console.log(error)
+            this.toastr.error(error.error)
           })
     }
 
     LogOut(){
       this.accountService.Logout();
+      this.router.navigateByUrl('/')
     }
 }
